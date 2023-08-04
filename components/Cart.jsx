12b208db8028b31c28d5eb9376a2bd78aa38959e
benkahla-karim <Benkahla-karim.bildozer2018@gmail.com>
2023-08-04 16:13:15 +0100

    sulution in img tag

--- components/Cart.jsx
+++ components/Cart.jsx
@@ -33,8 +33,7 @@ const Cart = () => {
         <div className='product-container'>
           {cartItems.length >= 1 && cartItems.map((item ,index) =>  (
             <div className='product' key={item._id}>
-              {/*<img src={urlfor(item.image[0])} className='cart-product-image' />*/}
-              <img src={urlfor(item.image[0])} width={20} height={20} />
+              <img src={urlfor(item.image[0])} className='cart-product-image' />
               <div className='item-desc'>
                 <div className='flex top'>
                   <h5>{item.name}</h5>
